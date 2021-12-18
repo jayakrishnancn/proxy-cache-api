48390e5dd836d9d8e421bc47f1370d0c66267cec
jayakrishnan cn <jayakrishnancn@gmail.com>
2021-12-18 09:51:59 +0530

    tmp

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,49 @@
-import { getHeaders } from "./serverUtils";
+import fs from "fs";
+import {
+  getFileName,
+  getHeaders,
+  getRequestBody,
+  shouldCacheRequest,
+  stringToFileName,
+} from "./serverUtils";
 
 var http = require("http"),
   httpProxy = require("http-proxy");
 
 const proxy = httpProxy.createProxyServer({ target: "http://localhost:9000" });
-proxy.on("proxyReq", function (proxyReq, req, res, options) {});
+
+proxy.on("proxyReq", function (proxyReq, req, res, options) {
+  if (!shouldCacheRequest(req)) {
+    return;
+  }
+  proxyReq.end();
+  // set cors headers
+  const resHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "*",
+    "Access-Control-Max-Age": 2592000, // 30 days
+  };
+
+  if (req.method === "OPTIONS") {
+    res.writeHead(204, resHeaders);
+    res.end();
+    return;
+  }
+  getRequestBody(req);
+  req.on("end", () => {
+    const fileName = getFileName(req);
+    // const fileContent = getFileContent(fileName, req.stringBody);
+    if (true) {
+      console.log("writing to file", fileName);
+
+      res.end();
+    }
+  });
+  // console.log("RAW Request from the source", JSON.stringify(fileName, null, 2));
+});
 
 proxy.on("proxyRes", function (proxyRes, req, res) {
-  console.log(
-    "RAW Response from the target",
-    JSON.stringify(getHeaders(proxyRes), null, 2)
-  );
+  // cache files
 });
 //
 
@@ -21,12 +54,17 @@ proxy.listen(8000);
 //
 http
   .createServer(function (req, res) {
+    console.log("triggered remote server");
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.write(
-      "request successfully proxied!" +
-        "\n" +
-        JSON.stringify(req.headers, null, 2)
-    );
-    res.end();
+    getRequestBody(req);
+    req.on("end", () => {
+      res.write(
+        "request successfully proxied!" +
+          "\n" +
+          JSON.stringify(req.headers, null, 2) +
+          JSON.stringify(req.stringBody, null, 2)
+      );
+      res.end();
+    });
   })
   .listen(9000);
--- src/serverUtils.ts
+++ src/serverUtils.ts
@@ -1,7 +1,9 @@
+import { MD5 } from "./hash";
+
 export const getHeaders = (proxyRes: any) => {
   const rawHeaders = proxyRes.rawHeaders;
   return rawHeaders
-    .filter((_, i) => i % 2 === 0)
+    ?.filter((_, i) => i % 2 === 0)
     .reduce(
       (acc, curr, i) => ({
         ...acc,
@@ -9,4 +11,34 @@ export const getHeaders = (proxyRes: any) => {
       }),
       {}
     );
+};
+
+export const getRequestBody = (req: any) => {
+  let body = "";
+  req.on("data", (chunk) => {
+    body += chunk;
+  });
+  req.on("end", () => {
+    var result;
+    try {
+      result = JSON.parse(body);
+    } catch (ex) {
+      result = "";
+    }
+    req.stringBody = result;
+  });
+};
+
+export const stringToFileName = (s: string): string =>
+  s.replace(/[^a-z0-9-]/gi, "_").toLowerCase();
+
+export const shouldCacheRequest = (req: any) =>
+  true ||
+  ["GET"].includes(req.method) ||
+  req.headers["content-type"] === "application/json";
+
+export const getFileName = (req: any) => {
+  const headers = JSON.stringify(getHeaders(req));
+  const hash = MD5(headers + "-" + req.stringBody);
+  return stringToFileName(`${req?.url}-${req?.method}-${hash}.json`);
 };
